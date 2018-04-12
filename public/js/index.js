6b57e02b963a11c60c4e42f275ce301786c4007d
davidlzs <davidlzs@hotmail.com>
2018-04-11 21:03:24 -0600

    diable button when sending location

--- public/js/index.js
+++ public/js/index.js
@@ -22,13 +22,14 @@ socket.on('newLocationMessage', function(message) {
   jQuery('#messages').append(li);
 });
 
+var messageTextBox = jQuery('[name=message]');
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function() {
-
+    messageTextBox.val('');
   });
 });
 
@@ -37,13 +38,16 @@ locationButton.on('click', function(e) {
   if(!navigator.geolocation) {
     return alert('Geolocation not supported by your borwser.');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
     console.log(position);
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fecth location');
   })
 });
