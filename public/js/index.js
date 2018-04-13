3b56ff97ef4c76a6414dc07824b81d59c3fd17a0
davidlzs <davidlzs@hotmail.com>
2018-04-13 17:45:09 -0600

    scroll to bottom if user is close to the bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
 var socket = io();
+
+function scrollToBottom() {
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -7,7 +23,6 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function(message) {
-  console.log('New message ', message);
   // var formattedTime = moment(message.createdAt).format("h:mm a");
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -21,6 +36,7 @@ socket.on('newMessage', function(message) {
     });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -39,6 +55,7 @@ socket.on('newLocationMessage', function(message) {
     });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 var messageTextBox = jQuery('[name=message]');
